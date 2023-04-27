3e553f1cc84ba6f5cc640da530e393dd575f7295
asm <vaskocuturilo@rambler.ru>
2023-04-27 19:10:19 +0200

    DV-00003: add center text

---
+++ js/script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const btnNew = document.querySelector('.btn--new');
+
+
+btnNew.addEventListener('click', () => {
+   alert('We\'ll start killing orcs very soon. But not now.');
+})
