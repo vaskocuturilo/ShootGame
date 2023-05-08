3c2b07b08bb8db8ca188dfb5eec155f4f115b9f4
asm <vaskocuturilo@rambler.ru>
2023-05-08 19:39:24 +0200

    DV-00008: Add bullet hole.

--- js/script.js
+++ js/script.js
@@ -16,11 +16,15 @@ const soldier3 = document.querySelector('#soldier-3');
 
 const fire =  document.getElementById('fire');
 const audio = document.createElement("AUDIO");
+const bulletHole = document.querySelector('.bullet-hole');
+const bloodSpot = document.querySelector('.blood-spot');
 
 //modal window
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const close = document.querySelector('.close-modal');
+
+bloodSpot.style.display = 'none';
 
 document.body.appendChild(audio);
 audio.src = "./sound/battle.ogg";
@@ -47,8 +51,10 @@ btnNew.addEventListener('click', () => {
   soldier2.classList.remove('hidden');
   soldier3.classList.remove('hidden');
   scoreContainer.classList.remove('hidden');
+  bulletHole.classList.remove('hidden');
+  bloodSpot.classList.remove('hidden');
   btnNew.classList.add('hidden');
-  modal.classList.add('hidden');
+  modal.classList.add('hidden');  
 })
 
 btnEdit.addEventListener('click', ()=> {
@@ -79,7 +85,33 @@ const increaseScore = () => {
   }
 };
 
-function shoot(zombie){
-  zombie.style.display = 'none';
-  increaseScore();
+function shoot(zombie) {
+    fire.play();
+    zombie.style.display = 'none';
+    increaseScore();
+    bloodSpot.style.display='block';
+    setTimeout(()=>{
+    bloodSpot.style.display='none';
+  }, 1000);
 }
+
+window.addEventListener('mousemove',(e)=> {
+  bulletHole.style.left = e.pageX + "px";
+  bulletHole.style.top = e.pageY + "px"; 
+})
+
+window.addEventListener('mousemove',(e)=> {
+  bloodSpot.style.left = e.pageX + "px";
+  bloodSpot.style.top = e.pageY + "px"; 
+})
+
+bulletHole.style.display = 'none';
+window.addEventListener('click', (e) => {
+  fire.play();
+  bulletHole.style.display='block';
+  setTimeout(()=>{
+    bulletHole.style.display='none';
+  }, 1000);
+})
+
+
